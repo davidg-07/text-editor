7a8ddcaaab9e207d0a5234a31d8ed3562e2e6520
David Galvez <david.dmg17@gmail.com>
2022-09-29 09:32:58 -0700

    created install button and finsihed install

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,34 +13,47 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
 
-// Creates connection to database
-const contactDb = await openDB('jate', 1);
+  // Creates connection to database
+  const jateDb = await openDB('jate', 1);
 
-// Creates a new transaction and specify  the database and data privileges
-const tx = contactDb.transaction('jate', 'readwrite');
+  // Creates a new transaction and specify  the database and data privileges
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .add() method on the store and pass in the content.
+  const request = store.put({ id: 1, value: content });
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
+};
+;
 
 // Export a function we will use to GET to the database.
 export const getDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('contact', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('contact', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
 
   // Get confirmation of the request.
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result.value);
+  return result.value;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'jate'
       }),
 
       // Injects our custom service worker
@@ -34,8 +34,8 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
+        name: 'just another text editor',
+        short_name: 'jate',
         description: 'Never forget your contacts!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
